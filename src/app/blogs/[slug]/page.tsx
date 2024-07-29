48f9151f1d63d62be35d9127eea5fbbf47e34214
eyupkirci <eyupkirci@gmail.com>
2024-07-29 14:31:24 +0200

    refactor: develop blog html render without dangerouslySetInnerHTML

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -3,12 +3,76 @@
 import Image from 'next/image';
 
 import Card from '@/app/components/Card';
-import type { ICard } from '@/app/types';
+import type { ICard, IHTMLContent } from '@/app/types';
 import { PreprSdk } from '@/server/prepr';
 
 export default async function BlogDetail({ params }: { params: { slug: string } }) {
   const { Blog } = await PreprSdk.BlogBySlug({ slug: params.slug });
-  const { Similar_Blogs } = await PreprSdk.Similar_Blogs({ limit: 3, similarBlogsId: Blog._id });
+  const { Similar_Blogs } = await PreprSdk.Similar_Blogs({ limit: 3, similarBlogsId: Blog?._id });
+
+  // Show the blog HTML rendered on a safe way, without dangerouslySetInnerHTML
+
+  /**
+   * This funcstion render blog HTML on a safe way, without dangerouslySetInnerHTML Instead of using
+   * this function, html-react-parser package can be imported
+   *
+   * @param section: IHTMLContent
+   * @param index: Number
+   */
+
+  const renderHTMLTextContent = (section: IHTMLContent, index: number) => {
+    if (section.html.startsWith('<p>')) {
+      return (
+        <p className="font-[200]" key={index}>
+          {section.text}
+        </p>
+      );
+    } else if (section.html.startsWith('<h1>')) {
+      return (
+        <h1 className="text-9xl" key={index}>
+          {`H2 - ${section.text}`}
+        </h1>
+      );
+    } else if (section.html.startsWith('<h2>')) {
+      return (
+        <h2 className="text-5xl" key={index}>
+          {`H2 - ${section.text}`}
+        </h2>
+      );
+    } else if (section.html.startsWith('<h3>')) {
+      return (
+        <h3 className="text-4xl" key={index}>
+          {`H3 - ${section.text}`}
+        </h3>
+      );
+    } else if (section.html.startsWith('<h4>')) {
+      return (
+        <h4 className="text-3xl" key={index}>
+          {`H4 - ${section.text}`}
+        </h4>
+      );
+    } else if (section.html.startsWith('<h5>')) {
+      return <h5 className="text-xl" key={index}>{`H5 - ${section.text}`}</h5>;
+    } else if (section.html.startsWith('<ul>')) {
+      return (
+        <ul className="list-inside list-disc" key={section.text}>
+          {section.html
+            .split('<ul>')[1]
+            ?.split('</ul>')[0]
+            ?.split('<li>')
+            .filter((item) => item.endsWith('</li>'))
+            .map((item) => item.split('</li>')[0])
+            .map((item) => (
+              <li className="mx-5 text-sm" key={item?.toString()}>
+                {item}
+              </li>
+            ))}
+        </ul>
+      );
+    } else {
+      return <div key={index}>Problem with HTML string</div>;
+    }
+  };
 
   return (
     <article className="flex min-h-screen flex-col justify-between text-black dark:bg-white dark:text-black">
@@ -21,10 +85,8 @@ export default async function BlogDetail({ params }: { params: { slug: string } 
 
       <div className="relative h-[450] w-full overflow-hidden">
         <Image
-          src={Blog?.banner_image?.url as string}
+          src={Blog?.banner_image.url as string}
           alt={Blog?.title ?? ''}
-          // width={800}
-          // height={400}
           fill={true}
           objectFit="cover"
           objectPosition="top"
@@ -37,13 +99,13 @@ export default async function BlogDetail({ params }: { params: { slug: string } 
             {Blog?.categories[0]?.slug}
           </span>
 
-          <h1 className="my-6 text-4xl font-bold dark:text-black">{Blog?.title}</h1>
+          <h1 className="my-6 text-5xl font-black dark:text-black">{Blog?.title}</h1>
 
           <div className="w-full">
             {Blog?.content?.map((item, index) => (
-              <p key={`index-${index + 1}`} className="my-2 dark:text-gray-700">
-                {item?.text}
-              </p>
+              <div className="py-2" key={index}>
+                {renderHTMLTextContent(item as IHTMLContent, index)}
+              </div>
             ))}
           </div>
         </div>
@@ -55,9 +117,9 @@ export default async function BlogDetail({ params }: { params: { slug: string } 
       <section className="w-full bg-[#efeff8] p-6">
         <h2 className="mt-12 py-5 text-3xl dark:text-black md:text-5xl">Gerelateerde blogs</h2>
 
-        {Similar_Blogs?.items?.length > 1 ? (
+        {Similar_Blogs && Similar_Blogs.items.length > 1 ? (
           <div className="mx-auto flex w-full max-w-screen-2xl flex-col gap-12 py-5 md:m-auto md:flex-row">
-            {Similar_Blogs?.items.map((item) => (
+            {Similar_Blogs.items.map((item) => (
               <Card key={item._id} data={item as unknown as ICard} />
             ))}
           </div>
