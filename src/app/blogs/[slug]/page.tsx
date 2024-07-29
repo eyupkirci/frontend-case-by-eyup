add96a37a776cf1b4a0452e436f941702237645e
eyupkirci <eyupkirci@gmail.com>
2024-07-29 17:28:54 +0200

    refactor: customHTMLTextContentRenderer function to show blog html rendering

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -6,73 +6,82 @@ import Card from '@/app/components/Card';
 import type { ICard, IHTMLContent } from '@/app/types';
 import { PreprSdk } from '@/server/prepr';
 
+// Show the blog HTML rendered on a safe way, without dangerouslySetInnerHTML
+/**
+ * This funcstion render blog HTML on a safe way, without dangerouslySetInnerHTML Instead of using
+ * this function, html-react-parser package can be imported but keept as much as dependency-free
+ *
+ * @param section:{text:string, Html:string}
+ * @param index: Number
+ * @returns JSX Element
+ */
+
+const customHTMLTextContentRenderer = (section: IHTMLContent, index: number) => {
+  //checks if it is an html element by checking first and last char
+  if (!section.html.startsWith('<') && !section.html.endsWith('>')) {
+    return <p key={index}>Problem with HTML string</p>;
+  }
+
+  if (section.html.startsWith('<p>')) {
+    return (
+      <p className="font-[200]" key={index}>
+        {section.text}
+      </p>
+    );
+  } else if (section.html.startsWith('<h1>')) {
+    return (
+      <h1 className="text-9xl" key={index}>
+        {`H1 - ${section.text}`}
+      </h1>
+    );
+  } else if (section.html.startsWith('<h2>')) {
+    return (
+      <h2 className="text-5xl" key={index}>
+        {`H2 - ${section.text}`}
+      </h2>
+    );
+  } else if (section.html.startsWith('<h3>')) {
+    return (
+      <h3 className="text-4xl" key={index}>
+        {`H3 - ${section.text}`}
+      </h3>
+    );
+  } else if (section.html.startsWith('<h4>')) {
+    return (
+      <h4 className="text-3xl" key={index}>
+        {`H4 - ${section.text}`}
+      </h4>
+    );
+  } else if (section.html.startsWith('<h5>')) {
+    return <h5 className="text-xl" key={index}>{`H5 - ${section.text}`}</h5>;
+  } else if (section.html.startsWith('<h6>')) {
+    return (
+      <h6 className="text-sm" key={index}>
+        {`H6 - ${section.text}`}
+      </h6>
+    );
+  } else if (section.html.startsWith('<ul>')) {
+    return (
+      <ul className="list-inside list-disc" key={section.text}>
+        {section.html
+          .split('<ul>')[1]
+          ?.split('</ul>')[0]
+          ?.split('<li>')
+          .filter((item) => item.endsWith('</li>'))
+          .map((item) => item.split('</li>')[0])
+          .map((item) => (
+            <li className="mx-5 text-sm" key={item?.toString()}>
+              {item}
+            </li>
+          ))}
+      </ul>
+    );
+  }
+};
+
 export default async function BlogDetail({ params }: { params: { slug: string } }) {
   const { Blog } = await PreprSdk.BlogBySlug({ slug: params.slug });
   const { Similar_Blogs } = await PreprSdk.Similar_Blogs({ limit: 3, similarBlogsId: Blog?._id });
-
-  // Show the blog HTML rendered on a safe way, without dangerouslySetInnerHTML
-
-  /**
-   * This funcstion render blog HTML on a safe way, without dangerouslySetInnerHTML Instead of using
-   * this function, html-react-parser package can be imported
-   *
-   * @param section: IHTMLContent
-   * @param index: Number
-   */
-
-  const renderHTMLTextContent = (section: IHTMLContent, index: number) => {
-    if (section.html.startsWith('<p>')) {
-      return (
-        <p className="font-[200]" key={index}>
-          {section.text}
-        </p>
-      );
-    } else if (section.html.startsWith('<h1>')) {
-      return (
-        <h1 className="text-9xl" key={index}>
-          {`H2 - ${section.text}`}
-        </h1>
-      );
-    } else if (section.html.startsWith('<h2>')) {
-      return (
-        <h2 className="text-5xl" key={index}>
-          {`H2 - ${section.text}`}
-        </h2>
-      );
-    } else if (section.html.startsWith('<h3>')) {
-      return (
-        <h3 className="text-4xl" key={index}>
-          {`H3 - ${section.text}`}
-        </h3>
-      );
-    } else if (section.html.startsWith('<h4>')) {
-      return (
-        <h4 className="text-3xl" key={index}>
-          {`H4 - ${section.text}`}
-        </h4>
-      );
-    } else if (section.html.startsWith('<h5>')) {
-      return <h5 className="text-xl" key={index}>{`H5 - ${section.text}`}</h5>;
-    } else if (section.html.startsWith('<ul>')) {
-      return (
-        <ul className="list-inside list-disc" key={section.text}>
-          {section.html
-            .split('<ul>')[1]
-            ?.split('</ul>')[0]
-            ?.split('<li>')
-            .filter((item) => item.endsWith('</li>'))
-            .map((item) => item.split('</li>')[0])
-            .map((item) => (
-              <li className="mx-5 text-sm" key={item?.toString()}>
-                {item}
-              </li>
-            ))}
-        </ul>
-      );
-    } else {
-      return <div key={index}>Problem with HTML string</div>;
-    }
-  };
 
   return (
     <article className="flex min-h-screen flex-col justify-between text-black dark:bg-white dark:text-black">
@@ -81,7 +90,8 @@ export default async function BlogDetail({ params }: { params: { slug: string } 
         - Show a thumbnail at the top of the page
         - Show the tags above the blog title
         - Show a title
-        - Show the excerpt */}
+        - Show the excerpt 
+        - Show the blog HTML rendered on a safe way, without dangerouslySetInnerHTML */}
 
       <div className="relative h-[450] w-full overflow-hidden">
         <Image
@@ -104,7 +114,7 @@ export default async function BlogDetail({ params }: { params: { slug: string } 
           <div className="w-full">
             {Blog?.content?.map((item, index) => (
               <div className="py-2" key={index}>
-                {renderHTMLTextContent(item as IHTMLContent, index)}
+                {customHTMLTextContentRenderer(item as IHTMLContent, index)}
               </div>
             ))}
           </div>
